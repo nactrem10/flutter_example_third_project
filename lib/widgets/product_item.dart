35a2abe67dbea26421e3e111b1b95a723caef7e6
mertcankirtay <kirtay.mertcan@gmail.com>
2020-04-23 11:51:44 +0300

    end of section 10

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../screens/product_detail_screen.dart';
-import '../providers/product.dart';
 import '../providers/cart.dart';
+import '../providers/product.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   @override
